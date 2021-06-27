8996ab3cbda85664ea516bcbd72cd049ec709a94
diazflo <bdiazflo@everis.com>
2021-06-26 21:17:51 -0500

    update adicion de checkstyle y eurekaclient

--- src/main/java/com/everis/transactionservice/TransactionServiceApplication.java
+++ src/main/java/com/everis/transactionservice/TransactionServiceApplication.java
@@ -1,8 +1,14 @@
 package com.everis.transactionservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@Slf4j
+@EnableCircuitBreaker
+@EnableEurekaClient
 @SpringBootApplication
 public class TransactionServiceApplication {
 
